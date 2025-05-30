0c488a7ba116874c80ab2985c261073a0cd1c3cc
leo <leo@Apple-MacBook-Air.local>
2025-05-30 09:44:08 +0800

    202505300935

--- nvim/.config/nvim/lua/core/option.lua
+++ nvim/.config/nvim/lua/core/option.lua
@@ -78,7 +78,7 @@ vim.o.cursorline = true
 vim.o.colorcolumn = '80'
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.o.scrolloff = 0
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
