9ace821d6f03543eb49c9566bf93e863d2e62376
Gavin <flyleom@gmail.com>
2025-08-16 23:11:05 +0800

    Update .zshrc.zsh

--- zsh/.zshrc.zsh
+++ zsh/.zshrc.zsh
@@ -1,5 +1,23 @@
 
-# PROMPT='%F{yellow}%n@%m%f %F{cyan}%D{%H:%M}%f %F{green}%~%f %F{blue}❯%f '
+# HISTFILE="$HOME/.zsh_history"
+# HISTSIZE=10000
+# SAVEHIST=10000
+# setopt APPEND_HISTORY
+# setopt INC_APPEND_HISTORY
+# setopt SHARE_HISTORY
+# setopt HIST_IGNORE_SPACE
+# setopt EXTENDED_HISTORY
+# setopt HIST_IGNORE_DUPS
+# setopt HIST_REDUCE_BLANKS
+
+# PROMPT="%F{yellow}%n@%m%f %F{cyan}%D{%H:%M}%f %F{green}%~%f %F{blue}❯%f "
+
+# eval "$(zoxide init zsh)"
+# source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+# source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+# alias cd="z"
+# alias ls="eza"
 
 # ─────────────────────────────────────────────────────────────────────────────
 # starship
